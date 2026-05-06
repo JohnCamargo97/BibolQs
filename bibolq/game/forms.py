3910b71c9d0e4bd10d216fc776902b903e6c2503
JohnCamargo97 <john-alex131@hotmail.com>
2026-05-06 18:45:57 -0500

    template with form

--- bibolq/game/forms.py
+++ bibolq/game/forms.py
@@ -3,8 +3,11 @@ from .models import Chapter, Verse
 
 
 class VersePhraseSelect(forms.ModelForm):
+
+    CHAPTERS = Chapter.objects.all().values_list('name', 'name')
+    Chapter = forms.ChoiceField(widget=forms.Select, choices=CHAPTERS)
     IncompletePhrase = forms.CharField(max_length=1000)
     class Meta:
         model = Verse
-        fields = ('IncompletePhrase',)
+        fields = ('Chapter', 'NumberVerse' ,'ContentVerse' ,'IncompletePhrase')
     #IncompleteVerse = forms.CharField(max_length=1000, label='Verso Incompleto', required=True)
--- bibolq/game/models.py
+++ bibolq/game/models.py
@@ -4,8 +4,16 @@ from django.db import models
 class Chapter(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Verse(models.Model):
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     NumberVerse = models.SmallIntegerField(default=0)
     ContentVerse = models.CharField(max_length=1000)
     IncompletePhrase = models.CharField(max_length=1000, default="")
+
+    def __str__(self):
+        chapter = str(self.chapter)
+        NumberVerse = str(self.NumberVerse)
+        return chapter + ":" +NumberVerse
